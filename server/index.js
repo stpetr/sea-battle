8d1df38c5ab5d9518308005f5d22aea2450ac3a5
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-08-12 16:00:42 +0700

    Init server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const cors = require(`cors`)
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketIo = require('socket.io')
+
+require('./db/mongoose')
+
+const PORT = process.env.PORT
+const publicDirectoryPath = path.join(__dirname, '../public')
+const app = express()
+const httpServer = http.createServer(app)
+const io = socketIo(httpServer)
+
+app.use(cors())
+app.use(express.static(publicDirectoryPath))
+app.use(express.json())
+
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join(publicDirectoryPath, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log('Server is up on port', PORT)
+})
