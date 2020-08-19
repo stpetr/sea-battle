68c73e9e9957166d3b0604ec1c571627bfc03176
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-08-19 12:32:20 +0700

    Client: updated AppRouter. Added new components, replaced Router with BrowserRouter, removed history since BrowserRouter does not support it anymore

--- client/routers/AppRouter.js
+++ client/routers/AppRouter.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import PublicRoute from './PublicRoute'
 import HomePage from '../components/HomePage'
+import RegisterPage from '../components/RegisterPage'
+import LoginPage from '../components/LoginPage'
+import DashboardPage from '../components/DashboardPage'
 import NotFoundPage from '../components/NotFoundPage'
 
-export const history = createBrowserHistory()
-
 const AppRouter = () => (
-    <Router history={history}>
+    <BrowserRouter>
         <Switch>
             <PublicRoute path="/" component={HomePage} exact={true} />
+            <PublicRoute path="/register" component={RegisterPage} />
+            <PublicRoute path="/login" component={LoginPage} />
+            <PrivateRoute path="/dashboard" component={DashboardPage} />
             <Route component={NotFoundPage} />
         </Switch>
-    </Router>
+    </BrowserRouter>
 )
 
 export default AppRouter
