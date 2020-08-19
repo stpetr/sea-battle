15bbe495995961341d5ba96398c427acd0e6379c
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-08-19 12:21:38 +0700

    Client: added auth actions and reducer

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -1,11 +1,79 @@
+import { makeApiRequest } from '../helpers/fetch'
+
+export const AUTHENTICATE = 'AUTHENTICATE'
 export const LOGIN = 'LOGIN'
 export const LOGOUT = 'LOGOUT'
 
-export const login = (uid) => ({
+export const authenticate = (user) => ({
+    type: AUTHENTICATE,
+    user,
+})
+
+export const login = (user, token) => ({
     type: LOGIN,
-    uid
+    user,
+    token
 })
 
 export const logout = () => ({
     type: LOGOUT
 })
+
+export const beginAuthenticate = () => (dispatch) => {
+    makeApiRequest('user/check-auth', {
+        method: 'POST',
+    }, dispatch).then((data) => {
+        if (data && data.user) {
+            dispatch(authenticate(data.user))
+        }
+    })
+}
+
+export const beginLogin = (email, password) => (dispatch) => {
+    return makeApiRequest('user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            email,
+            password
+        })
+    }, dispatch).then((data) => {
+        if (data && data.user && data.token) {
+            return dispatch(login(data.user, data.token))
+        }
+
+        throw new Error('Failed to login')
+    })
+}
+
+export const beginLoginAsAnonymous = () => (dispatch) => {
+    return makeApiRequest('user/login-as-anonymous', {
+        method: 'POST',
+    }).then((data) => {
+        if (data && data.user && data.token) {
+            return dispatch(login(data.user, data.token))
+        }
+
+        throw new Error('Failed to login')
+    })
+}
+
+export const beginRegister = (user) => (dispatch) => {
+    return makeApiRequest('user/register', {
+        method: 'POST',
+        body: JSON.stringify(user)
+    }).then((data) => {
+        if (data && data.user && data.token) {
+            return dispatch(login(data.user, data.token))
+        }
+
+        throw new Error('Failed to register')
+    })
+}
+
+export const beginLogout = () => (dispatch) => {
+    return makeApiRequest('user/logout', {
+        method: 'POST',
+    }, dispatch).then(() => {
+        dispatch(logout())
+    })
+}
--- client/reducers/auth.js
+++ client/reducers/auth.js
@@ -1,16 +1,38 @@
 import {
+    AUTHENTICATE,
     LOGIN,
     LOGOUT
 } from '../actions/auth'
 
+const tokenKey = 'token'
 const defaultState = {}
+
 export default (state = defaultState, action) => {
     switch (action.type) {
+        case AUTHENTICATE:
+            if (action.user) {
+                return {
+                    user: action.user,
+                }
+            }
+
+            localStorage.setItem(tokenKey, '')
+
+            return state
         case LOGIN:
+            if (action.user) {
+                localStorage.setItem(tokenKey, action.token)
+                return {
+                    user: action.user
+                }
+            }
+
             return {
-                uid: action.uid
+                defaultState
             }
         case LOGOUT:
+            localStorage.setItem(tokenKey, '')
+
             return defaultState
         default:
             return state
