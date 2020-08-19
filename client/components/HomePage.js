ec81c964d432d605426bf4cb9732d06948b62718
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-08-19 12:28:39 +0700

    Client: added links to register and login pages on home page

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export class HomePage extends React.Component {
     render() {
         return (
             <div className="page page__home">
-                <h1 className="page__heading">Welcome!</h1>
+                <h1 className="page__heading">Welcome to our home page!</h1>
+                <p>
+                    Please <Link to="/register">sign up</Link> or <Link to="/login">sign in</Link> if you already have an account to play the game
+                </p>
             </div>
         )
     }
