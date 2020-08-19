717cc80daf3614534e2b71e660140620f08d710a
Petr Dvukhrechensky <petr.sdkb@gmail.com>
2020-08-19 12:37:37 +0700

    Client: init authentication in app.js

--- client/app.js
+++ client/app.js
@@ -3,16 +3,20 @@ import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
+import { beginAuthenticate } from './actions/auth'
 
 import 'normalize.css/normalize.css'
 import './styles/main.less'
 
 const store = configureStore()
-
+const rootElement = document.querySelector('#app')
 const tpl = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 )
 
-ReactDom.render(tpl, document.querySelector('#app'))
+ReactDom.render(tpl, rootElement)
+
+// Check if user has valid token in local storage
+store.dispatch(beginAuthenticate())
