cd133b4206061e0b12fa63878060f60e8f9d9583
Peter Dvukhrechensky <petr.sdkb@gmail.com>
2024-01-12 15:00:48 +0700

    Replace banner image with jpg version

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import bannerImage from 'assets/images/battleship.png'
+import bannerImage from 'assets/images/battleship.jpg'
 
 export const HomePage = () => {
   return (
